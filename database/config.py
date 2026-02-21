b1a16b2c573fb0ef3ed463cc9f5f12aa8577b8aa
misho <misho.niki2003@gmail.com>
2026-02-21 21:44:52 +0200

    initial

--- database/config.py
+++ database/config.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("postgresql+psycopg2://postgres-user:password@localhost:5432/scraper_db", echo=True)
+engine = create_engine("", echo=True)
 SessionLocal = sessionmaker(bind=engine)
